0c9e4013d71c21ac9cbeaf7a46b578446c3d8b68
MerlinAlex(HJP) <merlinalex@qq.com>
2022-06-21 22:17:57 +0800

    chore: setup eslint

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import Vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite'
 import AutoImport from 'unplugin-auto-import/vite'
 import UnoCSS from 'unocss/vite'
-import { presetAttributify, presetUno, presetIcons } from 'unocss'
+import { presetAttributify, presetIcons, presetUno } from 'unocss'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
